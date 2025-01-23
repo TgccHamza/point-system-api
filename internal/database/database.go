e7501a7b37c0422ebf320cf9c59b4fee1539a766
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2025-01-23 17:21:04 +0100

    .

--- internal/database/database.go
+++ internal/database/database.go
@@ -40,7 +40,7 @@ var (
 	dbname     = "blueprint"
 	password   = "password1234"
 	username   = "melkey"
-	port       = 33060
+	port       = "33060"
 	host       = "localhost"
 	dbInstance *service
 )
