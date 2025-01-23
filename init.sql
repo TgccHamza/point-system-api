a966902ff6dd89924aa6888fb623e6450436fcf6
Hamza Moukhlis <hamz.moukhlis@gmail.com>
2025-01-23 18:00:37 +0100

    .

--- init.sql
+++ init.sql
@@ -1,7 +1,7 @@
 CREATE USER 'melkey'@'%' IDENTIFIED BY 'password1234';
 GRANT ALL PRIVILEGES ON blueprint.* TO 'melkey'@'%';
 
-CREATE USER 'melkey'@'::1' IDENTIFIED BY 'password1234';
-GRANT ALL PRIVILEGES ON blueprint.* TO 'melkey'@'::1';
+-- CREATE USER 'melkey'@'::1' IDENTIFIED BY 'password1234';
+-- GRANT ALL PRIVILEGES ON blueprint.* TO 'melkey'@'::1';
 
 FLUSH PRIVILEGES;
